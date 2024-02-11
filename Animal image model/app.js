003ab5044c01aa1997823e22a57eecd2e6b814ae
nkornien <nickkornienko@outlook.com>
2024-02-10 20:29:30 -0800

    import assignment

---
+++ Animal image model/app.js
@@ -0,0 +1,52 @@
+let model;
+const labels = ["Cat","Dog","Elephant","Horse","Lion"]
+
+async function loadModel() {
+  model = await tf.loadLayersModel("model/model.json");
+  console.log("Model loaded!");
+}
+
+async function predict(imageElement) {
+  console.log("Predicting...");
+  if (!model) {
+    console.warn("Model not loaded, please wait...");
+    return;
+  }
+
+  const tensor = tf.browser
+    .fromPixels(imageElement)
+    .resizeNearestNeighbor([224, 224])
+    .toFloat();
+
+  const batched = tensor.expandDims(0);
+  const normalized = batched.div(255);
+  const prediction = await model.predict(normalized).data();
+
+  displayPrediction(prediction);
+}
+
+function displayPrediction(predictionArray) {
+  const highestProbabilityIndex = predictionArray.indexOf(
+    Math.max(...predictionArray)
+  );
+  const predictedClass = labels[highestProbabilityIndex];
+  document.getElementById(
+    "predictionResult"
+  ).innerText = `Predicted class: ${predictedClass} with probability ${predictionArray[highestProbabilityIndex]}`;
+}
+
+function handleUpload() {
+  console.log("Uploading image...");
+  const imageUpload = document.getElementById("imageUpload");
+  const reader = new FileReader();
+
+  reader.onload = async (e) => {
+    const img = new Image();
+    img.src = e.target.result;
+    img.onload = () => predict(img);
+  };
+
+  reader.readAsDataURL(imageUpload.files[0]);
+}
+
+loadModel();
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CMPE-258-Teachable-machines
 Demonstrating image, pose, and audio classification using teachable machines
+
+
+Image: Animal image model
+
+Pose: Yoga pose model
+
+Audio: Guitar audio model https://teachablemachine.withgoogle.com/models/rREDr579I/
+
+You can run the app using: python -m http.server within the coresponding folder, audio app should be run via teachable machines website.
